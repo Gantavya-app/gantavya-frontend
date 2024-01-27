c0443243ef826faf3d5d6710d4b3506b89a27ea7
SandeshGC <gcsandesh01@gmail.com>
2024-01-27 11:19:27 +0545

    add: navigation bar at the bottom of the application

--- src/utils/Layout.jsx
+++ src/utils/Layout.jsx
@@ -1,15 +1,57 @@
 import React from "react"
-import { StyleSheet, View } from "react-native"
+import { StyleSheet, Text, View } from "react-native"
+import { AntDesign } from "@expo/vector-icons"
 
 export default function Layout({ children }) {
-	return <View style={styles.container}>{children}</View>
+  return (
+    <View style={styles.container}>
+      <View>{children}</View>
+
+      <View style={styles.footer_navigation}>
+        <View style={styles.footer_icon_container}>
+          <AntDesign name="home" size={24} color="black" />
+          <Text>Home</Text>
+        </View>
+        <View style={styles.footer_icon_container}>
+          <AntDesign name="find" size={24} color="black" />
+          <Text>Explore</Text>
+        </View>
+        <View style={styles.footer_icon_container}>
+          <AntDesign name="search1" size={24} color="black" />
+          <Text>Identify</Text>
+        </View>
+        <View style={styles.footer_icon_container}>
+          <AntDesign name="staro" size={24} color="black" />
+          <Text>Saved</Text>
+        </View>
+        <View style={styles.footer_icon_container}>
+          <AntDesign name="setting" size={24} color="black" />
+          <Text>Settings</Text>
+        </View>
+      </View>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "#fff",
-		alignItems: "center",
-		justifyContent: "center",
-	},
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "start",
+    width: "auto",
+  },
+  footer_navigation: {
+    flexDirection: "row",
+    alignSelf: "stretch",
+    justifyContent: "space-around",
+    marginTop: "auto",
+    gap: 2,
+    paddingVertical: 10,
+    borderTopColor: "rgba(0,0,0,0.1)",
+    borderTopWidth: 1,
+  },
+  footer_icon_container: {
+    alignItems: "center",
+  },
 })

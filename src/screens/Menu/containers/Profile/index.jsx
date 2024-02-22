784dcb6f84a48791f0157ef5c9ee4aa5ca59328c
SandeshGC <gcsandesh01@gmail.com>
2024-02-22 11:25:45 +0545

    add: contents to profile page

--- src/screens/Menu/containers/Profile/index.jsx
+++ src/screens/Menu/containers/Profile/index.jsx
@@ -1,10 +1,67 @@
-import React from "react"
-import { Text, View } from "react-native"
+import React, { useContext, useState } from "react"
+import { Text, TextInput, TouchableOpacity, View } from "react-native"
+import { AuthContext } from "../../../../contexts/AuthContext"
 
-const ProfileScreen = () => {
+const ProfileScreen = ({ navigation }) => {
+  const { user } = useContext(AuthContext)
+
+  const [userEditing, setUserEditing] = useState(user)
+  const [editMode, setEditMode] = useState(false)
+
+  console.warn(user)
+
   return (
     <View>
-      <Text>ProfileScreen</Text>
+      {!editMode ? (
+        <View>
+          <Text>{user?.name}</Text>
+        </View>
+      ) : (
+        <View>
+          <TextInput
+            value={userEditing?.name}
+            onChangeText={(text) =>
+              setUserEditing({
+                ...userEditing,
+                name: text,
+              })
+            }
+          />
+        </View>
+      )}
+      {!editMode ? (
+        <View>
+          <Text>{user?.email}</Text>
+        </View>
+      ) : (
+        <View>
+          <TextInput
+            value={userEditing?.email}
+            onChangeText={(text) =>
+              setUserEditing({
+                ...userEditing,
+                email: text,
+              })
+            }
+          />
+        </View>
+      )}
+
+      {!editMode ? (
+        <TouchableOpacity onPress={() => setEditMode(true)}>
+          <Text>Edit Profile</Text>
+        </TouchableOpacity>
+      ) : (
+        <View>
+          <TouchableOpacity onPress={() => setEditMode(false)}>
+            <Text>Save Profile</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={() => setEditMode(false)}>
+            <Text>Cancel</Text>
+          </TouchableOpacity>
+        </View>
+      )}
     </View>
   )
 }
